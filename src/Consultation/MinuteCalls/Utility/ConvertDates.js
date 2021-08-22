d24ae6104441f8b916cd6fdb865e0853ac99947c
perriex <parna80as@gmail.com>
2021-08-22 12:27:29 +0430

    date buf handled

--- src/Consultation/MinuteCalls/Utility/ConvertDates.js
+++ src/Consultation/MinuteCalls/Utility/ConvertDates.js
@@ -3,6 +3,7 @@ import { DateFormatter } from "./DateFormatter";
 export const ConvertDates = (arr) => {
   arr.forEach((obj) =>
     Object.keys(obj).forEach(function (key) {
+      if (key === "doneDateTime" && obj[key] === null) return;
       if (key === "startDateTime" || key === "doneDateTime") {
         let date = new Date(obj[key]);
         let jdate = new JDate(date);
