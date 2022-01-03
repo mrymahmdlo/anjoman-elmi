d6b5ed6b5e23fd53d450dbe5395be2bfdbaeb5c9
Bright <monirehghandani@gmail.com>
2022-01-04 03:20:34 +0330

    edit time sheet

--- src/TimeSheet/CreateTimeSheet/Components/FormItems.js
+++ src/TimeSheet/CreateTimeSheet/Components/FormItems.js
@@ -1,80 +1,34 @@
-// import { FormSwitchInput, FormTextInput } from "src/reusable/FormInput";
-//
-// const { CSelect } = require("@coreui/react");
-//
-// export const FormItems = (form, setForm, groupIds, courseIds) => {
-//   return [
-//     {
-//       name: "عنوان محتوای عمومی",
-//       text: "عنوان محتوای خود را وارد کنید",
-//       input: FormTextInput(form, setForm, "title", "عنوان"),
-//       size: 6,
-//     },
-//     {
-//       name: "زمان تقریبی مطالعه مقاله",
-//       text: "زمان تقریبی به دقیقه برای خواندن این محتوا را تعیین کنید.",
-//       input: FormTextInput(form, setForm, "timeToStudy", "زمان مطالعه"),
-//       size: 6,
-//     },
-//     {
-//       name: "مقطع تحصیلی",
-//       text: "مقطع تحصیلی مخاطب این محتوا را وارد کنید",
-//       input: (
-//         <CSelect
-//           custom
-//           name="groupId"
-//           style={{ width: "100%" }}
-//           defaultValue={form.groupId}
-//           value={form.groupId}
-//           onChange={(e) => setForm({ ...form, groupId: +e.target.value })}
-//         >
-//           <option value={0}>همه</option>
-//           {groupIds.map((item, key) => (
-//             <option
-//               key={key}
-//               value={item.id}
-//               selected={form.groupId === item.id}
-//             >
-//               {item.name}
-//             </option>
-//           ))}
-//         </CSelect>
-//       ),
-//       size: 6,
-//     },
-//     {
-//       name: "درس",
-//       text: "درس مربوطه به این محتوا را انتخاب کنید",
-//       input: (
-//         <CSelect
-//           custom
-//           name="courseId"
-//           style={{ width: "100%" }}
-//           defaultValue={form.courseId}
-//           value={form.courseId}
-//           onChange={(e) => setForm({ ...form, courseId: +e.target.value })}
-//         >
-//           <option value={0}>همه</option>
-//           {courseIds.length > 0
-//             ? courseIds.map((item, key) => (
-//               <option
-//                 key={key}
-//                 value={item.id}
-//                 selected={form.courseId === item.id}
-//               >
-//                 {item.name}
-//               </option>
-//             ))
-//             : null}
-//         </CSelect>
-//       ),
-//       size: 6,
-//     },
-//     {
-//       name: "محتوای مهم",
-//       text: "در صورت فعال شدن، در محتواهای اصلی سایت نمایش داده میشود",
-//       input: FormSwitchInput(form, setForm, "isImportant", form.isImportant),
-//       size: 6,
-//     },
-//   ];
-// };
+import { FormNumberInput } from "src/reusable/FormInput";
+
+export const FormItems = (form, setForm) => {
+  return [
+    {
+      name: "زمان شروع",
+      text: "زمان شروع را وارد کنید",
+      input: FormNumberInput(
+        form,
+        setForm,
+        "startPeriodHour",
+        "زمان شروع",
+        0,
+        null,
+        () => form.startPeriodHour >= "0"
+      ),
+      size: 4,
+    },
+    {
+      name: "زمان پایان",
+      text: "زمان پایان را وارد کنید",
+      input: FormNumberInput(
+        form,
+        setForm,
+        "endPeriodHour",
+        "زمان پایان",
+        0,
+        null,
+        () => form.endPeriodHour >= "0"
+      ),
+      size: 4,
+    },
+  ];
+};
--- src/TimeSheet/EditTimeSheet/EditTimeSheet.js
+++ src/TimeSheet/EditTimeSheet/EditTimeSheet.js
@@ -1,109 +1,81 @@
-// import React, { useEffect, useState } from "react";
-// import {
-//   CButton,
-//   CCard,
-//   CCardFooter,
-//   CCardHeader,
-//   CContainer,
-//   CSpinner,
-// } from "@coreui/react";
-// import CIcon from "@coreui/icons-react";
-// import { GetData, postFormData } from "src/Service/APIEngine";
-// import { Toast } from "src/Utility/Toast";
-// import ArticleForm from "src/Content/CreateArticle/Components/ArticleForm";
-// import { useHistory, useParams } from "react-router";
-// import { ChangeValues } from "./Components/ChangeValues";
-// import { GetDotNetGeorgianFromDateJS } from "src/Utility/DateTime";
-//
-// const EditArticle = () => {
-//   const { id } = useParams();
-//   const [form, setForm] = useState({});
-//   const [showError, setShowError] = useState(false);
-//   const [errorContent, setErrorContent] = useState("");
-//   const [btnActice, setBtnActive] = useState(false);
-//   const history = useHistory();
-//
-//   useEffect(() => {
-//     setErrorContent("تا بارگزاری داده ها کمی صبر کنید");
-//     setShowError(true);
-//     GetData("FreeContent/GetFreeContent?contentId=" + id)
-//       .then((res) => {
-//         setForm(ChangeValues(res.data));
-//       })
-//       .finally(() => {
-//         setShowError(false);
-//       });
-//   }, [id]);
-//
-//   const submitContent = () => {
-//     setShowError(false);
-//     setBtnActive(true);
-//     const now = new Date();
-//
-//     if (!form.writerProviderId) delete form["writerProviderId"];
-//     if (!form.image) {
-//       delete form["image"];
-//     }
-//     if (form.Image !== "") {
-//       form["image"] = form["Image"];
-//     }
-//
-//     form["group"] = form["groupId"];
-//     form["course"] = form["courseId"];
-//     delete form["groupId"];
-//     delete form["courseId"];
-//     delete form["Image"];
-//     delete form["imageLink"];
-//
-//     postFormData("FreeContent/EditFreeContent", {
-//       ...form,
-//       createdDateTime: GetDotNetGeorgianFromDateJS(now),
-//     })
-//       .then(() => {
-//         setErrorContent("داده با موفقیت ثبت شد ");
-//         setShowError(true);
-//         history.push("/Content/FreeContent/ManageArticles");
-//         setBtnActive(false);
-//       })
-//       .catch(() => {
-//         setErrorContent("خطا در ثبت ویرایش");
-//         setShowError(true);
-//         setBtnActive(false);
-//       });
-//   };
-//
-//   return (
-//     <div className="App">
-//       <CContainer fluid>
-//         <CCard>
-//           <CCardHeader>
-//             ویرایش محتوای
-//             <small> متنی(مقاله)</small>
-//           </CCardHeader>
-//           <ArticleForm form={form} setForm={setForm} preData={form.imageLink} />
-//           <CCardFooter>
-//             {!btnActice ? (
-//               <CButton
-//                 type="submit"
-//                 size="sm"
-//                 color="primary"
-//                 onClick={submitContent}
-//               >
-//                 <CIcon name="cil-scrubber" /> ثبت محتوا
-//               </CButton>
-//             ) : (
-//               <CSpinner
-//                 style={{ width: "2rem", height: "2rem" }}
-//                 color=""
-//                 variant="grow"
-//               />
-//             )}
-//           </CCardFooter>
-//         </CCard>
-//       </CContainer>
-//       <Toast showError={showError} errorContent={errorContent} />
-//     </div>
-//   );
-// };
-//
-// export default EditArticle;
+import React, { useEffect, useState } from "react";
+import {
+  CButton,
+  CCard,
+  CCardFooter,
+  CCardHeader,
+  CContainer,
+  CSpinner,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import {  PostDataProvider } from "src/Service/APIProvider";
+import { Toast } from "src/Utility/Toast";
+import TimeSheetForm from '../CreateTimeSheet/Components/TimeSheetForm';
+
+const EditTimeSheet = ({ obj, setModal }) => {
+  const [form, setForm] = useState({});
+  const [showError, setShowError] = useState(false);
+  const [errorContent, setErrorContent] = useState("");
+  const [btnActive, setBtnActive] = useState(false);
+
+  useEffect(() => {
+    console.log('obj',obj)
+    setErrorContent("تا بارگزاری داده ها کمی صبر کنید");
+    setShowError(true);
+    setForm(obj);
+  }, [obj]);
+
+  const submitContent = () => {
+    setShowError(false);
+    setBtnActive(true);
+    PostDataProvider(`TimeSheet/EditTimeSheet?timeSheetId=${obj.timeSheetId}`, {
+      startPeriodHour: form.startPeriodHour,
+      endPeriodHour: form.endPeriodHour,
+      weekDay: form.weekDay,
+    })
+      .then(() => {
+        setErrorContent("داده با موفقیت ثبت شد ");
+        // history.push('/TimeSheet/ManageTimeSheet');
+        setShowError(true);
+        setBtnActive(false);
+        setModal(false);
+      })
+      .catch(() => {
+        setErrorContent("خطا در ثبت ویرایش");
+        setShowError(true);
+        setBtnActive(false);
+      });
+  };
+
+  return (
+    <div className="App">
+      <CContainer fluid>
+        <CCard>
+          <CCardHeader>ویرایش زمان بندی</CCardHeader>
+          <TimeSheetForm form={form} setForm={setForm} />
+          <CCardFooter>
+            {!btnActive ? (
+              <CButton
+                type="submit"
+                size="sm"
+                color="primary"
+                onClick={submitContent}
+              >
+                <CIcon name="cil-scrubber" /> ثبت زمان بندی
+              </CButton>
+            ) : (
+              <CSpinner
+                style={{ width: "2rem", height: "2rem" }}
+                color=""
+                variant="grow"
+              />
+            )}
+          </CCardFooter>
+        </CCard>
+      </CContainer>
+      <Toast showError={showError} errorContent={errorContent} />
+    </div>
+  );
+};
+
+export default EditTimeSheet;
