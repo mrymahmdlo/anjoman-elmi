9c253727150910421553f5be699024eb7347719e
M.Ahmadlo <ahmdlomrym@gmail.com>
2022-01-17 13:03:03 +0330

    commit

--- src/Tutoring/ProviderCourse/CreateProviderCourse/SelectProvider.js
+++ src/Tutoring/ProviderCourse/CreateProviderCourse/SelectProvider.js
@@ -1,8 +1,22 @@
 import { CFormGroup, CSelect } from "@coreui/react";
 import { useEffect, useState } from "react";
 import { PostData } from "src/Service/APIEngine";
+import Select from "react-select";
 
 export const SelectProvider = ({ providerId, setProviderId }) => {
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      height: 35,
+      width: 340,
+      minHeight: 35,
+    }),
+    option: (provided) => ({
+      ...provided,
+      textAlign: "right",
+    }),
+  };
+
   const [providers, setProviders] = useState([]);
 
   useEffect(() => {
@@ -13,23 +27,14 @@ export const SelectProvider = ({ providerId, setProviderId }) => {
   return (
     <CFormGroup>
       <label>ارائه دهنده</label>
-      <CSelect
-        value={providerId}
-        onChange={(e) =>
-         setProviderId(Number(e.target.value))
-        }
-      >
-        <option value={-1}>ارائه دهنده را انتخاب کنید</option>
-        {providers.length > 0 ? (
-          providers.map((item) => (
-            <option value={item.providerId} key={item.providerId}>
-              {item.name + " " + item.lastName}
-            </option>
-          ))
-        ) : (
-          <option>پشتیبانی وجود ندارد</option>
-        )}
-      </CSelect>
+      <Select
+        options={providers.map((opt) => ({
+          label: opt.name + " " + opt.lastName,
+          value: opt.name + " " + opt.lastName,
+        }))}
+        defaultValue={{ label: "ارائه دهنده را انتخاب کنید" }}
+        styles={customStyles}
+      />
     </CFormGroup>
   );
 };
