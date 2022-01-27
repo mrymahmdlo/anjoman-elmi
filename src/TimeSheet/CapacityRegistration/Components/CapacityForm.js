c491e23f8178aa5ed4ccce2e3f4882db46136dcd
M.Ahmadlo <ahmdlomrym@gmail.com>
2022-01-27 16:03:49 +0330

    ch

--- src/TimeSheet/CapacityRegistration/Components/CapacityForm.js
+++ src/TimeSheet/CapacityRegistration/Components/CapacityForm.js
@@ -7,39 +7,38 @@ import {
   CCol,
   CSelect,
 } from "@coreui/react";
+import { GetDataProvider } from "src/Service/APIProvider";
 import { FormItems } from "./FormItems";
 import { TextField } from "src/Utility/InputGroup";
 import { PostData } from "src/Service/APIEngine";
 
 const CapacityForm = ({ form, setForm }) => {
   const [providers, setProviders] = useState([]);
-
+  const [providerId, setProviderId] = useState(-1);
 
   const items = FormItems(form, setForm).map((item) => TextField(item));
   useEffect(() => {
     PostData("Provider/Tutoring", {}).then((res) => {
-    setProviders(res.data);
+      setProviders(res.data);
     });
   }, []);
 
   return (
     <CCardBody>
       <CForm action="" method="post">
-      
         <CRow>
-        
           <CCol sm={6}>
             <CFormGroup>
               <label htmlFor="nf-title"> پشتیبان ها : </label>
 
               <CSelect
                 value={form.providerId}
-                defaultValue={-1}
+                defaultValue={providerId}
                 onChange={(e) => {
                   setForm({ ...form, providerId: e.target.value });
                 }}
               >
-                <option value={0}>پشتیبان را انتخاب کنید</option>
+                <option value={-1}>پشتیبان را انتخاب کنید</option>
                 {providers.length > 0 ? (
                   providers.map((item) => (
                     <option value={item.providerId} key={item.providerId}>
@@ -51,8 +50,8 @@ const CapacityForm = ({ form, setForm }) => {
                 )}
               </CSelect>
             </CFormGroup>
-            </CCol>
-            <CCol sm={6}>{items}</CCol>
+          </CCol>
+          <CCol sm={6}>{items}</CCol>
         </CRow>
       </CForm>
     </CCardBody>
