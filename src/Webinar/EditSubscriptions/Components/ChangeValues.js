7289c7346cba5f43750894278247d786f8e6990f
M.Ahmadlo <ahmdlomrym@gmail.com>
2022-01-29 21:45:08 +0330

    change

--- src/Webinar/EditSubscriptions/Components/ChangeValues.js
+++ src/Webinar/EditSubscriptions/Components/ChangeValues.js
@@ -1,13 +1,10 @@
 export const ChangeValues = (obj) => {
   let newobj = {
     // webinarLink: obj.webinarLink,
-    schedules: [
-      {
-        buyDateTime: obj.webinarSchedules[0].buyDateTime,
-        joinDatetime: obj.webinarSchedules[0].joinDatetime,
-        cancelDatetime: obj.webinarSchedules[0].cancelDatetime,
-      },
-    ],
+
+    buyDateTime: obj.webinarSchedules[0].buyDateTime,
+    joinDatetime: obj.webinarSchedules[0].joinDatetime,
+    cancelDatetime: obj.webinarSchedules[0].cancelDatetime,
   };
 
   return newobj;
